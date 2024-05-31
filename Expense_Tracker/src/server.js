d0fad6684fd205505ecbabc452baa0abcbbf87f1
MariaTech2024 <mariavolzhina@gmail.com>
2024-05-31 15:40:06 +0300

    expense_tracker

--- Expense_Tracker/src/server.js
+++ Expense_Tracker/src/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const transactionRoutes = require('./routes/transactionRoutes.js');
-const path = require('path');
 
 const app = express();
 const port = 3004;
@@ -15,11 +15,4 @@ app.use('/transactions', transactionRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-});
-
-
-app.use(express.static(path.join(__dirname, "/public/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public/build", "index.html"));
 });
